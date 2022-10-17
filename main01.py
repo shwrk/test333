9a55cdb144adf7ef7267987cadeb401ea07e70f6
shwrk <razxca@icloud.com>
2022-10-17 10:28:47 +0300

    changes

---
+++ main01.py
@@ -0,0 +1,14 @@
+from random import randint
+
+N = 10
+a = []
+for i in range(N):
+    a.append(randint(15, 88))
+print(a)
+
+for i in range(N - 1):
+    for j in range(N - i - 1):
+        if a[j] > a[j + 1]:
+            a[j], a[j + 1] = a[j + 1], a[j]
+
+print(a)
