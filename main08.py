f6eab00cec8265d60480bf91bbe322b53249a057
shwrk <razxca@icloud.com>
2022-10-29 18:03:07 +0300

    operator in

---
+++ main08.py
@@ -0,0 +1,6 @@
+message = "hello world!"
+hello = "hello"
+print(hello in message)  # True - подстрока hello есть в строке "hello world!"
+
+gold = "gold"
+print(gold in message)  # False - подстроки "gold" нет в строке "hello world!"
