7507522e504cdc719733c77dea66418056c769ba
shwrk <razxca@icloud.com>
2022-10-29 17:18:07 +0300

    globalnye localnye peremennye

---
+++ main03.py
@@ -0,0 +1,13 @@
+def say_hi():
+    name = "Sam"
+    surname = "Johnson"
+    print("Hello", name, surname)
+
+
+def say_bye():
+    name = "Tom"
+    print("Good bye", name)
+
+
+say_hi()
+say_bye()
