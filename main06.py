ae37c473e01836db21aadfb990311cbc4daf4696
shwrk <razxca@icloud.com>
2022-10-29 17:47:10 +0300

    bool lesson

---
+++ main06.py
@@ -0,0 +1,11 @@
+a = 5
+b = 6
+result = 5 == 6  # сохраняем результат операции в переменную
+print(result)  # False - 5 не равно 6
+print(a != b)  # True
+print(a > b)  # False - 5 меньше 6
+print(a < b)  # True
+
+bool1 = True
+bool2 = False
+print(bool1 == bool2)  # False - bool1 не равно bool2
